ba4207732e40b9a2b24a728316f6ffe43ff104aa
Dean Dierschow <ddierschow@xocolatl.com>
2014-07-04 07:04:26 +0200

    Completely rewrote the database page.

--- htdocs/database.php
+++ htdocs/database.php
@@ -8,17 +8,53 @@ DoHead($pif);
 $isadmin = CheckPerm('a');
 $pif['hier'][0] = array('/', 'Home');
 $pif['hier'][1] = array('/database.php', 'Database');
-if ($pif['fake'])
+$answer = Fetch("select min(year), max(year) from lineup_model", $pif);
+$yearstart = $answer[0][0];
+$yearend = $answer[0][1];
+
+$sections = array();
+$sections[] = array("tag" => "id", "name" => "Specific Model ID", "fn" => 'SectionID', "scr" => "msearch.cgi");
+$sections[] = array("tag" => "year", "name" => "Year", "fn" => 'SectionYear', "scr" => "lineup.cgi");
+$sections[] = array("tag" => "rank", "name" => "Lineup Number", "fn" => 'SectionRank', "scr" => "lineup.cgi");
+$sections[] = array("tag" => "manno", "name" => "MAN Number", "fn" => 'SectionManno', "scr" => "manno.cgi");
+$sections[] = array("tag" => "mack", "name" => "Mack Number", "fn" => 'SectionMack', "scr" => "mack.cgi");
+$sections[] = array("tag" => "makes", "name" => "Make", "fn" => 'SectionMakes', "scr" => "makes.cgi");
+$sections[] = array("tag" => "search", "name" => "Text Search", "fn" => 'SectionSearch', "scr" => "msearch.cgi");
+$sections[] = array("tag" => "vsearch", "name" => "Variation Text Search", "fn" => 'SectionVSearch', "scr" => "vsearch.cgi");
+$sections[] = array("tag" => "packs", "name" => "Multi-Model Packs", "fn" => 'SectionPacks', "scr" => "packs.cgi");
+$sections[] = array("tag" => "sets", "name" => "Special Sets", "fn" => 'SectionSets', "scr" => "matrix.cgi");
+
+DoResetJavascript();
+DoIncDecJavascript();
+DoPageHeader($pif);
+
+echo '<ul class="header-links">' . "\n";
+foreach ($sections as $sec)
 {
-    $yearstart = 1953;
-    $yearend = 2011;
+    echo '<li class="header-link-item"><a href="#' . $sec['tag'] . '">By ' . $sec['name'] . "</a></li>\n";
 }
-else
+echo "</ul>
+
+<table width=\"100%\">
+";
+
+foreach ($sections as $sec)
 {
-    $answer = Fetch("select min(year), max(year) from lineup_model", $pif);
-    $yearstart = $answer[0][0];
-    $yearend = $answer[0][1];
+    Section($sec);
 }
+
+echo "</table>\n<hr>\n";
+
+DoButtonLink("back", $imgdirArt, '/');
+?>
+to the index.
+<a href="comment.php?page=database"><img src="../pic/gfx/but_comment_on_this_page.gif" alt="COMMENT" onmouseover="this.src='../pic/gfx/hov_comment_on_this_page.gif';" onmouseout="this.src='../pic/gfx/but_comment_on_this_page.gif';" class="comment"></a>
+
+<?php
+DoPageFooter($pif);
+echo "</body>\n</html>\n";
+
+//---- support functions -----------------------------------------
 
 function SelectYear($name, $id, $defval, $min, $max)
 {
@@ -37,35 +73,25 @@ function SelectYear($name, $id, $defval, $min, $max)
     echo "<br>\n";
 }
 
-DoResetJavascript();
-DoIncDecJavascript();
-DoPageHeader($pif);
-echo "<!-- done page header -->";
-
-function SectionHeader($sect, $cgi, $title)
+function Section($args)
 {
     echo "
 <tr><td><br></td></tr>
-<tr><td class=\"" . $sect . "_head sel_head\">
-<a name=\"" . $sect . "\"></a>
-<center><h2>" . $title . "</h2></center>
+<tr><td class=\"" . $args['tag'] . "_head sel_head\">
+<a name=\"" . $args['tag'] . "\"></a>
+<center><h2>" . $args['name'] . "</h2></center>
 </td></tr>
 <tr><td class=\"spacer\"></td></tr>
 
-<tr><td class=\"" . $sect . "_body sel_body\">
-Select what kind of Matchbox lineup you would like, then click \"SEE THE MODELS\".<p>
+<tr><td class=\"" . $args['tag'] . "_body sel_body\">
+Select what kind of Matchbox lineup you would like to see, then click \"SEE THE MODELS\".<p>
 
-<form action=\"/cgi-bin/" . $cgi . "\" method=\"get\" name=\"" . $sect . "\">
+<form action=\"/cgi-bin/" . $args['scr'] . "\" method=\"get\" name=\"" . $args['tag'] . "\">
 ";
-}
-
-function SectionTail($sect)
-{
+    call_user_func($args['fn']);
     echo "<br>\n";
-    //echo '<input type="image" name="submit" id="' . $sect . 'Submit" src="../pic/gfx/but_see_the_models.gif" alt="SEE THE MODELS" onmouseover="this.src=\'../pic/gfx/hov_see_the_models.gif\';" onmouseout="this.src=\'../pic/gfx/but_see_the_models.gif\';" class="button"> - ';
     DoButtonSubmit("see_the_models", "../pic/gfx", "submit");
-    #echo '<img src="../pic/gfx/but_reset.gif" onmouseover="this.src=\'../pic/gfx/hov_reset.gif\';" onmouseout="this.src=\'../pic/gfx/but_reset.gif\';" border="0" onClick="ResetForm(document.' . $sect . ')" alt="RESET" class="button">';
-    DoButtonReset("../pic/gfx", $sect);
+    DoButtonReset("../pic/gfx", $args['tag']);
     echo "\n</form>\n\n</td></tr>\n";
 }
 
@@ -83,48 +109,31 @@ function ChooseRegion($nrows)
 Note that Australian dealers might carry either USA or International assortments after 2001.</td>
 <?php
 }
-?> 
 
-<!--The Interactive Matchbox Lineup-->
-<ul class="header-links">
-<li class="header-link-item"><a href="#id">By Specific MAN ID</a></li>
-<li class="header-link-item"><a href="#year">By Year</a></li>
-<li class="header-link-item"><a href="#rank">By Lineup Number</a></li>
-<li class="header-link-item"><a href="#manno">By MAN Number</a></li>
-<li class="header-link-item"><a href="#mack">By Mack Number</a></li>
-<li class="header-link-item"><a href="#makes">By Make</a></li>
-<li class="header-link-item"><a href="#search">By Text Search</a></li>
-<li class="header-link-item"><a href="#vsearch">By Variation Text Search</a></li>
-<li class="header-link-item"><a href="#packs">By Multi-Model Packs</a></li>
-<li class="header-link-item"><a href="#sets">By Special Sets</a></li>
-</ul>
+//---- beginning of sections -------------------------------------
 
-<table width="100%">
-
-<?php
-SectionHeader("id", "msearch.cgi", "Specific Manufacturing ID");
+function SectionID()
+{
 ?>
 <table>
 <tr>
 <td>See specific manufacturing ID:</td><td><input type="text" name="id" id="idId" value="" size="12"></td></tr>
 </table>
-
 <?php
-SectionTail('id');
+}
 
-SectionHeader("year", "lineup.cgi", "Models by Year");
+function SectionYear()
+{
+    global $yearstart, $yearend, $isadmin;
 ?>
 <table><tr>
 <td valign=top>Year: </td>
 <td valign=top class="updown">
 <?php SelectYear('year', 'yearYear', $yearend, $yearstart, $yearend); ?>
-
 </td>
-
 <td width=20></td>
 <?php
 ChooseRegion(1);
-
 if ($isadmin)
 {
     echo '<td width=20></td><td>';
@@ -136,11 +145,12 @@ if ($isadmin)
 ?>
 </tr>
 </table>
-
 <?php
-SectionTail('year');
+}
 
-SectionHeader("rank", "lineup.cgi", "Models by Lineup Number");
+function SectionRank()
+{
+    global $yearstart, $yearend, $isadmin;
 ?>
 <input type="hidden" name="n" value="1">
 <table><tr>
@@ -159,11 +169,9 @@ Start year:
 <td valign=top class="updown">
 <?php SelectYear('syear', 'rankSyear', $yearstart, $yearstart, $yearend); ?>
 </td>
-
 <td width=16 rowspan=2></td>
 <?php
 ChooseRegion(2);
-
 if ($isadmin)
 {
     echo '<td width=16 rowspan=2></td>';
@@ -200,33 +208,19 @@ End year:
 <?php SelectYear('eyear', 'rankEyear', $yearend, $yearstart, $yearend); ?>
 </td>
 </table>
-
 <?php
-SectionTail('rank');
+}
 
-SectionHeader("manno", "manno.cgi", "Manufacturing Numbers");
+function SectionManno()
+{
+    global $yearstart, $yearend, $isadmin, $pif;
 ?>
 <table>
 <tr><td colspan=7>
 <select name="section" id="manSection">
 <option value="" selected>Please select a range
 <?php
-if ($pif['fake'])
-{
-?>
-<option value="man">Main 1-75 Line
-<option value="rwr">Real Working Rigs
-<option value="mi">Older Matchbox International Castings
-<option value="orig">Matchbox Originals
-<option value="promo">Promotional Models
-<option value="wr">White Rose Only
-<option value="fea">Featured and Collectibles Castings
-<option value="sf">Superfast Models 1970-1981
-<option value="rn">Roman Numeral Models
-<option value="rw">Regular Wheel Models
-<?php
-}
-else if ($isadmin)
+if ($isadmin)
 {
     $sl = Fetch("select id, name from section where page_id like 'man%' order by display_order", $pif);
     foreach ($sl as $ent)
@@ -303,16 +297,14 @@ End year:
 ?>
 </tr>
 </table>
-
 Filter by vehicle type:
 <table>
 <tr>
 <td class="tdboth" colspan=3>Every vehicle has one of these.</td>
 <td class="tdboth" colspan=3>Vehicle may have up to two of these.</td>
 </tr>
-
 <?php
-function ynmcell($arr, $verb)
+function YNMCell($arr, $verb)
 {
     global $isadmin;
     echo('<td class="tdleft"><b>' . $arr[1] . "</b></td>");
@@ -326,10 +318,8 @@ function ynmcell($arr, $verb)
     echo('<input type="radio" name="type_' . $arr[0] . '" value="m" checked>maybe' . "\n");
     echo("</td>\n");
 }
-
 $a = array();
 $b = array();
-
 $a[] = array("a", "aircraft");
 $a[] = array("o", "boat");
 $a[] = array("b", "bus");
@@ -343,7 +333,6 @@ $a[] = array("z", "trailer");
 $a[] = array("t", "truck");
 $a[] = array("v", "van");
 $a[] = array("5", "wagon");
-
 $b[] = array("9", "ambulance");
 $b[] = array("c", "commercial");
 $b[] = array("i", "construction");
@@ -357,23 +346,19 @@ $b[] = array("l", "police");
 $b[] = array("8", "racer");
 $b[] = array("h", "recreation");
 $b[] = array("x", "taxi");
-
 foreach(array_keys($a) as $k)
 {
     echo("<tr>\n");
-    ynmcell($a[$k], $isadmin);
-    ynmcell($b[$k], $isadmin);
+    YNMCell($a[$k], $isadmin);
+    YNMCell($b[$k], $isadmin);
     echo("</tr>\n");
 }
+echo "</table>\n";
+}
 
-?>
-
-</table>
-
-<?php
-SectionTail('manno');
-
-SectionHeader("mack", "mack.cgi", '"Mack" Numbers');
+function SectionMack()
+{
+    global $isadmin;
 ?>
 <table><tr>
 <td><input type="radio" name="sect" value="all" checked> Both sections</td>
@@ -397,11 +382,11 @@ if ($isadmin)
 <?php incrnum('mackEnd', "document.getElementById('mackStart').value", 120, 'document.mack.range[1].checked=true;'); ?>
 </td></tr>
 </table>
-
 <?php
-SectionTail('mack');
+}
 
-SectionHeader("makes", "makes.cgi", "Vehicle Makes");
+function SectionMakes()
+{
 ?>
 Choose a make:<br>
 <table><tr><td valign="top">
@@ -412,18 +397,19 @@ Choose a make:<br>
 </td>
 <td valign="top">Or, <a href="../cgi-bin/makes.cgi">choose from a list!</a></td>
 </tr></table>
-
 <?php
-SectionTail('makes');
+}
 
-SectionHeader("search", "msearch.cgi", "Text Search");
+function SectionSearch()
+{
 ?>
 Search the casting information for: <input type="text" name="query"><br>
-
 <?php
-SectionTail('search');
+}
 
-SectionHeader("vsearch", "vsearch.cgi", "Variation Text Search");
+function SectionVSearch()
+{
+    global $isadmin;
 ?>
 Search the variation information for models containing the following.<p>
 <table><tr><td width=50 rowspan=6></td>
@@ -437,14 +423,15 @@ Search the variation information for models containing the following.<p>
 <tr><td>Windows:</td><td><input type="text" name="windows"></td></tr>
 <?php if ($isadmin) echo '<tr><td></td><td><i>Category:</i></td><td><input type="text" name="cat"></td></tr>'; ?>
 </table>
-
 <?php
-SectionTail('vsearch');
+}
 
-SectionHeader("packs", "packs.cgi", "Multi-Model Packs");
+function SectionPacks()
+{
+    global $isadmin, $pif;
 ?>
 <select name="page" id="packsPage">
-<option value="" selected>
+<option value="" selected>Please select a pack type.
 <?php
 $sl = Fetch("select id, flags, title from page_info where format_type='packs' order by title", $pif);
 foreach ($sl as $ent)
@@ -459,57 +446,20 @@ if ($isadmin)
 }
 ?>
 <br>
-
 <?php
-SectionTail('packs');
+}
 
-SectionHeader("sets", "matrix.cgi", "Special Sets");
+function SectionSets()
+{
+    global $isadmin, $pif;
 ?>
 <select name="page" id="setsPage">
-<option value="" selected>
+<option value="" selected>Please select a set.
 <?php
-if ($pif['fake'])
-{
-?>
-<option value="matrix.character">Character Cars - 1979 through 2000
-<option value="matrix.codered">Code Red TV Series - 1981
-<option value="matrix.supergt">Budget Range Models - 1986
-<option value="matrix.drt">Dragon Racing Team - 1986
-<option value="matrix.sf">SuperFast - 1986-1990
-<option value="matrix.scmw">Monster Trucks - 1986-1994
-<option value="matrix.lasers">Laser Wheels - 1988
-<option value="matrix.wclass">World Class Series - 1989-1995
-<option value="matrix.orig">40th Anniversary Originals - 1993
-<option value="matrix.cchoice">Collector's Choice - 1994
-<option value="matrix.goldcoin">Gold Coin Series - 1995
-<option value="matrix.nba">Matchbox NBA Collection - 1995-1998
-<option value="matrix.chall">Challenge Series - 1997
-<option value="matrix.starcars">Star Car Collection - 1998-1999
-<option value="matrix.fastfood">Fast Food Toys - 1999 onward
-<option value="matrix.germany">Germany Special Series - 2001-2009
-<option value="matrix.hs">Heroes Series - 2002-2003
-<option value="matrix.maa">Across America - 2003
-<option value="matrix.sf1">Superfast - 2004
-<option value="matrix.maw">Around the World - 2004
-<option value="matrix.sf2">Superfast - 2005
-<option value="matrix.sf3">Superfast - 2006
-<option value="matrix.sf4">Superfast - 2007
-<option value="matrix.bob07">Best of British - 2007
-<option value="matrix.boi08">Best of International - 2008
-<option value="matrix.bom08">Best of Muscle - 2008
-<option value="matrix.bob08">Best of British - 2008
-<option value="matrix.sf5">Superfast - 2008
-<option value="matrix.sf6">Superfast 40th Anniversary - 2009
-<option value="matrix.bob09">Best of British - 2009
-<?php
-}
-else
-{
-    $sl = Fetch("select id, flags, title, description from page_info where format_type='matrix' order by description", $pif);
-    foreach ($sl as $ent)
-	if (!($ent[1] & 1))
-	    echo '<option value="' . trim($ent[0]) . '">' . $ent[2] . " - " . $ent[3] . "\n";
-}
+$sl = Fetch("select id, flags, title, description from page_info where format_type='matrix' order by description", $pif);
+foreach ($sl as $ent)
+    if (!($ent[1] & 1))
+	echo '<option value="' . trim($ent[0]) . '">' . $ent[2] . " - " . $ent[3] . "\n";
 ?>
 </select>
 <?php incrsel('setsPage', -1);
@@ -519,20 +469,8 @@ if ($isadmin)
 }
 ?>
 <br>
-
 <?php
-SectionTail('sets');
+}
+
+//---- end of sections -------------------------------------------
 ?>
-
-<tr><td><hr></td></tr>
-</table>
-
-<a href=".."><img src="../pic/gfx/but_back.gif" class="button" alt="BACK" onmouseover="this.src='../pic/gfx/hov_back.gif';" onmouseout="this.src='../pic/gfx/but_back.gif';" style="vertical-align: middle; color: #FFFFFF; border-width: 0px; padding: 1px;"> to the main index.</a>
-<a href="comment.php?page=3inch"><img src="../pic/gfx/but_comment_on_this_page.gif" alt="COMMENT" onmouseover="this.src='../pic/gfx/hov_comment_on_this_page.gif';" onmouseout="this.src='../pic/gfx/but_comment_on_this_page.gif';" class="comment"></a>
-
-<?php
-DoPageFooter($pif);
-?>
-
-</body>
-</html>
