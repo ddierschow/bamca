f99e763da24d703807f64d1d776405d7eb41e8ca
Dean Dierschow <ddierschow@xocolatl.com>
2019-06-03 06:13:13 +0000

    Fix error in logs.

--- htdocs/bin/basics.php
+++ htdocs/bin/basics.php
@@ -1,4 +1,6 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT']);
+set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT'] . '/bin');
 include "config.php";
 include "db.php";
 
